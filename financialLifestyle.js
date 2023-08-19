c9225d93c29d1bedc510a59d8cebadc261598723
annabelle-zhang <annabellezh21@gmail.com>
2023-08-19 16:07:36 -0400

    hey

---
+++ financialLifestyle.js
@@ -0,0 +1,16 @@
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "Mozilla is cool, ${myName}";
+}
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla is cool, ${storedName}";
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
