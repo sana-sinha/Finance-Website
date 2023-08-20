a0ea8889b2d2cd512447ebf3c27419c175d42e99
annabelle-zhang <annabellezh21@gmail.com>
2023-08-20 17:28:40 -0400

    expenses

--- expenses.js
+++ expenses.js
@@ -1,5 +1,6 @@
-function returntext() {
+function returnText() {
     let name = document.getElementById("expensesName").value;
     let fee = document.getElementById("expensesValue").value;
     let rep = document.getElementById("expensesTime").value;
+    document.getElementById("expenses").innerHTML = name + fee + rep;
 }
