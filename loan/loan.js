00e82e355d71284be80b60440ef31ce898eb8f5e
sanasinha <sinhasana13@gmail.com>
2023-08-20 21:11:36 -0400

    hi

--- loan/loan.js
+++ loan/loan.js
@@ -1,6 +1,7 @@
 let allAmounts = [];
 let allTerms = [];
 let allInterests = [];
+let allMonthly = [];
 let allCompounds = [];
 let allPaybacks = [];
 
@@ -11,16 +12,18 @@ function returnText() {
     let amount = document.getElementById("loanAmount").value;
     let term = document.getElementById("loanTerm").value;
     let interest = document.getElementById("interestRate").value;
+    let monthly = document.getElementById("perMonth").value;
     let comp = document.getElementById("compound").value;
     let payb = document.getElementById("payback").value;
 
     allAmounts.push(amount);
     allTerms.push(term);
     allInterests.push(interest);
+    allMonthly.push(monthly);
     allCompounds.push(comp);
     allPaybacks.push(payb);
 
-    tot += "$" + amount + ", " + term + " month term, " + interest + "% interest, compounded " + comp + ", paid back every " + payb + "<br>";
+    tot += "$" + amount + ", " + term + " month term, " + interest + "% interest, $" + monthly + " paid monthly, compounded " + comp + ", paid back every " + payb + "<br>";
 
     document.getElementById("loans").innerHTML = tot;
 }
